d2d7d8d1be6b7ad3eb8fa69f4e66e4a4251c5042
nicolas-massaccese <nicolasmassaccese@gmail.com>
2022-12-02 00:41:39 -0300

    random undefined corregido

--- desafio3.js
+++ desafio3.js
@@ -13,67 +13,47 @@ class Container {
         this.fileName = fileName
     }
 
-
     getAll () {
         const fileData =  fs.readFileSync(`./${this.fileName}.txt`, 'utf-8');
         return (JSON.parse(fileData));
     }
-
 }
 
 
 const product = new Container ('products');
 
 
-    
-console.log(product.getAll());
-
 function showDataJason(showData){
-
     let dataHtml = '';
-
     showData.forEach(data => {
         dataHtml +=`<p>${data.id}</p>
         <p>${data.title}</p>
         <p>${data.price}</p>
-        <img src="${data.thumbnail}"  width="100" height="100">`
+        <img src="${data.thumbnail}" width="100" height="100">`
     });
     
-
     app.get('/products', (req, res) => {
-
         res.send(`${dataHtml}`);
     });
-
 }
 
-function showRandomDataJason(showData){
 
-    let dataHtml = '';
-
-    showData.forEach(data => {
-        dataHtml +=`<p>${data.id}</p>
-        <p>${data.title}</p>
-        <p>${data.price}</p>
-        <img src="${data.thumbnail}"  width="100" height="100">`
-    });
-
-
-     
+function showRandomDataJason(data){
+    let random = data[Math.floor(Math.random() * data.length)];
 
     app.get('/productRandom', (req, res) => {
-
-        res.send(`${dataHtml[Math.floor(Math.random() * dataHtml.lenght)]}`)
+        res.send(`<p>${random.id}</p>
+        <p>${random.title}</p>
+        <p>${random.price}</p>
+        <img src="${random.thumbnail}"  width="100" height="100">`)
     });
-
-}
+};
 
 
 const dataJason = product.getAll();
 
 showDataJason(dataJason)
 showRandomDataJason(dataJason)
-
 
 const PORT = 8080;
 
